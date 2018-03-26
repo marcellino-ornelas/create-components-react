7421da848d11b90659eebc90459d9d377a35ad57
marcellino-ornelas <marcellino.ornelas@gmail.com>
2018-03-26 15:34:52 -0700

    added support for path support

---
+++ header/Header.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import './Header.css';
+
+class Header extends Component {
+  // constructor(props){
+    // super();
+    // this.state = {};
+  // }
+
+  // componentWillMount(){}
+  // componentDidMount(){}
+  // componentWillUnmount(){}
+
+  // componentWillReceiveProps(){}
+  // shouldComponentUpdate(){}
+  // componentWillUpdate(){}
+  // componentDidUpdate(){}
+
+  render() {
+    return (
+      <div></div>
+    );
+  }
+}
+
+export default Header;
