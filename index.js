4137a5f37c014127d58c4866cfa1616223634879
marcellino-ornelas <marcellino.ornelas@gmail.com>
2018-03-27 08:53:33 -0700

    finished clean up

--- index.js
+++ index.js
@@ -9,14 +9,6 @@ const program = require("commander");
 
 program
   .version('0.1.0')
-  // .usage('[options] <file ...>')
-  .option('-i, --integer <d>', 'An integer argument', parseInt)
-  // .option('-f, --float <n>', 'A float argument', parseFloat)
-  // .option('-r, --range <a>..<b>', 'A range', range)
-  // .option('-l, --list <items>', 'A list', list)
-  // .option('-o, --optional [value]', 'An optional value')
-  // .option('-c, --collect [value]', 'A repeatable value', collect, [])
-  // .option('-v, --verbose', 'A value that can be increased', increaseVerbosity, 0)
   .parse(process.argv);
 
 // console.dir(program);
