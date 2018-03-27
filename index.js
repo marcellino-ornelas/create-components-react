b25efe1abf32b687003766447ee506ec757b0ef0
marcellino-ornelas <marcellino.ornelas@gmail.com>
2018-03-26 20:05:35 -0700

    add functionality for many components

--- index.js
+++ index.js
@@ -5,8 +5,22 @@
 */
 
 const createReactComponent = require("./lib");
-const name = process.argv[2];
+const program = require("commander");
+
+program
+  .version('0.1.0')
+  // .usage('[options] <file ...>')
+  .option('-i, --integer <d>', 'An integer argument', parseInt)
+  // .option('-f, --float <n>', 'A float argument', parseFloat)
+  // .option('-r, --range <a>..<b>', 'A range', range)
+  // .option('-l, --list <items>', 'A list', list)
+  // .option('-o, --optional [value]', 'An optional value')
+  // .option('-c, --collect [value]', 'A repeatable value', collect, [])
+  // .option('-v, --verbose', 'A value that can be increased', increaseVerbosity, 0)
+  .parse(process.argv);
+
+// console.dir(program);
 
 // Create component files
-createReactComponent( process.cwd(), name );
+createReactComponent( process.cwd(), program.args );
 
