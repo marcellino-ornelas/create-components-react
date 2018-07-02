532e38eb773bd580b322dfe71e7ac2a81bc5e8b1
Marcellino Ornelas <marcellino.ornelas.com>
2018-07-02 10:16:15 -0700

    added test for command line args

--- test/createReactComponentSpec.js
+++ test/createReactComponentSpec.js
@@ -1,55 +1,56 @@
-// /*
-//  * Node Modules
-// */
+/*
+ * Node Modules
+*/
 
-// const chai = require('chai');
-// const path = require('path');
-// const fs = require('fs-extra');
-// const async = require('async');
-// const child = require('child_process');
+const chai = require('chai');
+const path = require('path');
+const fs = require('fs-extra');
+const async = require('async');
+const child = require('child_process');
 
-// const cwd = __dirname;
+const cwd = __dirname;
+// const FILE_EXECUTE_PATH = path.join(__dirname, '..');
+// console.log(FILE_EXECUTE_PATH)
 
-
-// function spawn(args,done){
-//     child_process.execFile('node', args, {
-//       cwd: cwd,
-
-//     },function(err, stdout, stderr){
-//       done(err);
-//     });
-// }
+function spawn(args,done){
+    child.execFile(`node`, ['../index.js'].concat(args), {
+      cwd: cwd,
+    }, function(err, stdout, stderr){
+      done(err);
+    });
+}
 
 // function deleteFile( name, done ){
-//   fs.remove( path.join( cwd, name), done);
+//   fs.remove( name, done);
 // }
 
-// describe('Create React Component', function(){
+describe('Create React Component', function(){
 
-//   // it('Should not throw any errors',function( done ){
-//   //   spawn(['App'], done);
-//   // });
+  describe('Basic',function(){
+    
+    before(function(done){
+      basicComponents = ['Nav', 'App', 'Nav/NavItem'];
+      fullPathBasicComponents = basicComponents.map(function(name){ return path.join(cwd, name ) });
+      spawn( basicComponents, done);
+    })
 
-//   describe('Basic',function(){
-//     basicCompnents = ['Nav', 'App'];
-//     before(function(done){
-//       spawn( basicCompnents, done);
-//     })
+    after(function(done){
+      // delete file here
+      async.each(fullPathBasicComponents, fs.remove, done);
+    });
 
-//     it('should create correct files for all arguments', function(done){
-//       async.each( basicCompnents, fs.pathExists, function(err, exixts){
-//         if( err ) done(err);
-//       });
-//     });
+    it('should create correct files for all arguments', function(done){
+      async.each( 
+        fullPathBasicComponents,
+        fs.pathExists,
+        function(err, exixts){
+          if(err) console.log(err);
+          done(err);
+      });
 
+     });
 
-
-//     after(function(done){
-//       // delete file here
-//       deleteFile('App', done);
-//     });
-
-//   }) //Basic
+  }) //Basic
 
   // describe('Arguments',function(){
 
@@ -57,16 +58,12 @@
   //     spawn(['Nav', 'App'], done);
   //   })
 
-
-
   //   after(function(done){
   //     // delete file here
   //     deleteFile('App', done);
   //   });
   // })
 
-
-  
   // it('Should make a component with provided argument',function( done ){
   //   spawn(['App'], function(){
 
