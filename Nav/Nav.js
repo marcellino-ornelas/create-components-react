c2cb87822a9a6cf3a44af2c2a9f12901a2411bfe
Marcellino Ornelas <marcellino.ornelas.com>
2018-07-03 19:40:23 -0700

    added commands to commander. setting up settings for program

---
+++ Nav/Nav.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import './Nav.css';
+
+class Nav extends Component {
+  // constructor(props){
+    // super();
+    // this.state = {};
+  // }
+
+  // componentWillMount(){}
+  // componentDidMount(){}
+  // componentWillUnmount(){}
+
+  // componentWillReceiveProps(){}
+  // shouldComponentUpdate(){}
+  // componentWillUpdate(){}
+  // componentDidUpdate(){}
+
+  render() {
+    return (
+      <div></div>
+    );
+  }
+}
+
+export default Nav;
--- index.js
+++ index.js
@@ -4,13 +4,47 @@
  * Create React Component
 */
 
-const createReactComponent = require("./lib");
 const program = require("commander");
+const settings = require('./lib/settings');
+
 
 program
-  .version('0.1.0')
-  .parse(process.argv);
+  .version('2.0.3')
+  .option('-c, --css <ext>', 'change extention for css file')
 
+/*
+ * future
+*/
+// program.command('set <prop> <value>')
+//   .description('set a global setting')
+//   .action(function( prop, value, options){
+//     console.log('args in set: ', arguments);
+//   });
+
+settings.init();
+
+const createReactComponent = require("./lib");
+
+program
+  .command('create <components...>')
+  .alias('c')
+  .description('create a new component')
+  .action(function( files, options){
+    // console.log('args in create: ', options.args);
+    createReactComponent( process.cwd(), files );
+  });
+
+program
+  .command('* <components...>')
+  .action(function( files, options){
+    // console.log('args in *: ', options);
+    createReactComponent( process.cwd(), files );
+  });
+
+
+program.parse(process.argv);
+
+// console.log('anything else')
 // Create component files
-createReactComponent( process.cwd(), program.args );
+// createReactComponent( process.cwd(), program.args);
 
