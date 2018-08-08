386c8d526146a263db0a72dfe9390b730a0b7f5e
marcellino-ornelas <marcellino.ornelas.com>
2018-08-07 18:43:05 -0700

    adding support for local setting configurations

--- index.js
+++ index.js
@@ -4,13 +4,12 @@
  * Create React Component
 */
 
-const program = require("commander");
+const program = require('commander');
 const settings = require('./lib/settings');
-
 
 program
   .version('2.0.3')
-  .option('-c, --css <ext>', 'change extention for css file')
+  .option('-c, --css <ext>', 'change extention for css file');
 
 /*
  * future
@@ -21,28 +20,31 @@ program
 //     console.log('args in set: ', arguments);
 //   });
 
-const createReactComponent = require("./lib");
+const createReactComponents = require('./lib');
+
+program
+  .command('init')
+  .description('create local configuration settings for a repo to use')
+  .action(function(prop, value, options) {
+    console.log('initialize');
+  });
 
 program
   .command('create <components...>')
   .alias('c')
   .description('create a new component')
-  .action(function( files, options){
+  .action(function(files, options) {
     // console.log('args in create: ', options.args);
-    createReactComponent( process.cwd(), files );
+    createReactComponents(process.cwd(), files);
   });
 
-program
-  .command('* <components...>')
-  .action(function( files, options){
-    // console.log('args in *: ', options);
-    createReactComponent( process.cwd(), files );
-  });
-
+program.command('* <components...>').action(function(files, options) {
+  // console.log('args in *: ', options);
+  createReactComponents(process.cwd(), files);
+});
 
 program.parse(process.argv);
 
 // console.log('anything else')
 // Create component files
 // createReactComponent( process.cwd(), program.args);
-
