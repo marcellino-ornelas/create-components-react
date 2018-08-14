3d35352b95e88177b320bafac3ec035206c25e62
marcellino-ornelas <marcellino.ornelas.com>
2018-08-14 13:55:01 -0700

    fixed testing for component

--- test/componentSpec.test.js
+++ test/componentSpec.test.js
@@ -6,96 +6,35 @@ const fs = require('fs-extra');
 const path = require('path');
 const Component = require('../lib/component');
 
-
 const expect = chai.expect;
 
 const TESTING_ENV = __dirname;
 const MAX_FILES_FOR_COMPONENTS = 3;
 
 describe('Component', function() {
-  
   describe('initialization', function() {
-
-     beforeEach(function(){
+    beforeEach(function() {
       component = new Component(TESTING_ENV, 'App');
     });
 
     it('should create a new object', function() {
-      expect( component ).to.be.a('object');
+      expect(component).to.be.a('object');
     });
 
     it('should create a new object with propertys [ name, dir, fileStructure ]', function() {
-      expect( component.name ).to.be.a('string');
-      expect( component.dir ).to.be.a('string');
-      expect( component.fileStructure ).to.be.a('array');
+      expect(component.name).to.be.a('string');
+      expect(component.dir).to.be.a('string');
     });
 
-    it('Component name first charater should be capital letter', function() {
-      var firstCharOfComponentName = component.name[0]
-      expect( firstCharOfComponentName ).to.equal( firstCharOfComponentName.toUpperCase() );
-    });
+    it('Each name in component path should all be capitalized', function() {
+      var isCapitalized = component.name.split(path.sep).every(function(item) {
+        let firstCharOfComponentName = item[0];
+        return (
+          firstCharOfComponentName === firstCharOfComponentName.toUpperCase()
+        );
+      });
 
-    it('Component dir should be a full path', function() {
-      // expect( component.dir ).to.equal(  );
+      expect(isCapitalized).to.be.true;
     });
-
-    it('fileStructure should have correct number of files inside', function() {
-      expect( component.fileStructure ).to.have.lengthOf( MAX_FILES_FOR_COMPONENTS );
-    });
-
   }); // initialization
-
-  describe('Creation', function() {
-    describe('normal', function() {
-
-      before(function(done){
-        component = new Component(TESTING_ENV, 'App');
-        component.createComponent(done);
-      });
-
-      after(function(done){
-        fs.remove( component.dir, done );
-      });
-
-      it('should create a new component structure', function(done) {
-        expect( component.dir ).to.equal( path.join(__dirname, 'App') )
-        fs.pathExists( component.dir, function(err, exists){
-
-          done(err || !exists);
-
-        })
-      });
-
-      it('should have a index.js file', function(done) {
-
-        fs.pathExists( path.join(component.dir, 'index.js') , function(err, exists){
-
-          done(err || !exists);
-
-        });
-
-      });
-
-      it('should have a js file named from component name', function(done) {
-        fs.pathExists( path.join(component.dir, component.name + '.js') , function(err, exists){
-
-          done(err || !exists);
-
-        });
-      });
-
-      it('should contain a css file named from component name', function(done) {
-        fs.pathExists( path.join(component.dir,  component.name + '.css') , function(err, exists){
-
-          done(err || !exists);
-
-        });
-      });
-
-    }); //Normal
-
-    // describe('advanced', function(){}); // describe
-
-  }); // Creation
-
 }); // Component
